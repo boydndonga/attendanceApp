7fcce4d15e0cb8614f66dce0a5536c56e9f1bd7e
boyd ndonga <boydbugle@gmail.com>
2018-10-24 22:16:56 +0300

    create user model

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app import create_app,db
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User,Category,Project
 
-app = create_app('default')
+app = create_app('development')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
