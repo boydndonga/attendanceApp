adf3e10b2091767f87834e8b8f5cbd5863c5c683
boyd ndonga <boydbugle@gmail.com>
2018-10-27 19:15:19 +0300

    create function to generate avatar

--- app/models.py
+++ app/models.py
@@ -47,14 +47,16 @@ class User(db.Model):
         return True
 
     def gravatar_hash(self):
-        self.avatar_hash = hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
-        # return hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        return hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
 
     def gravatar(self, size=100, default='identicon', rating='g'):
         url = 'https://secure.gravatar.com/avatar'
-        hash = self.avatar_hash or self.gravatar_hash()
-        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+        hash = self.gravatar_hash()
+        self.avatar_hash =  '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
             url=url, hash=hash, size=size, default=default, rating=rating)
+        return self.avatar_hash
+
 
 class UserSchema(ma.Schema):
     class Meta:
