4bcd0056fbdebf7dfa29cc8dc40724098836182e
boyd ndonga <boydbugle@gmail.com>
2018-10-28 16:45:59 +0300

    create test for correct registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,14 +1,22 @@
 from . import auth
 from .. import db
-from ..models import User
+from ..models import User,user_schema
 from flask import request, jsonify, make_response
 
 
-# @auth.route('/register', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         return 'posted'
-#     return make_response(jsonify({'message':'make post request'}))
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data.get('username')
+        email = data.get('email')
+        pass_secure = data.get('password')
+        user = User(username=username, email=email, pass_secure=pass_secure)
+        user.gravatar()
+        db.session.add(user)
+        db.session.commit()
+        return make_response(jsonify({'message': 'successfully created user'}), 201)
+    return make_response(jsonify({'message':'make post request'}))
 
 
 @auth.route('/login', methods=['GET', 'POST'])
--- tests/test_auth_views.py
+++ tests/test_auth_views.py
@@ -1,29 +1,49 @@
-# import unittest
-# import json
-# from base64 import b64encode
-# from app import create_app
-#
-#
-# class AuthViewTESTCase(unittest.TestCase):
-#     def setUp(self):
-#         self.app = create_app('testing')
-#         self.app_context = self.app.app_context()
-#         self.app_context.push()
-#         self.client = self.app.test_client
-#
-#     @staticmethod
-#     def get_api_headers(email='', password=''):
-#         return {
-#             'Authorization':
-#                 'Basic' + b64encode((email + ':' + password).encode('utf-8')).decode('utf-8'),
-#             'Accept': 'application/json',
-#             'Content-Type': 'application/json'
-#         }
-#
-#     def get_client_request(self, email='app@test.com', password='appytesty', path='/authenticate/register'):
-#         user_data = {'email': email, 'password': password}
-#         return self.client().post(
-#             path,
-#             headers=self.get_api_headers(),
-#             data=json.dumps(user_data)
-#         )
+import unittest
+import json
+from base64 import b64encode
+from app import create_app, db
+from app.models import Role, User
+
+
+class AuthViewTESTCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client
+        self.new_role = Role(name='Admin')
+        db.create_all()
+
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def save_role(self):
+        db.session.add(self.new_role)
+        db.session.commit()
+
+    @staticmethod
+    def get_api_headers(email='', password=''):
+        return {
+            # 'Authorization':
+            #     'Basic' + b64encode((email + ':' + password).encode('utf-8')).decode('utf-8'),
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+
+    def get_client_request(self, username='testsy', email='app@test.com', password='appytesty', path='/authenticate/register'):
+        user_data = {'username':username, 'email': email, 'password': password}
+        return self.client().post(
+            path,
+            headers=self.get_api_headers(),
+            data=json.dumps(user_data)
+        )
+
+    def test_auth_registration(self):
+        # test successful registration
+        res = self.get_client_request()
+        result = json.loads(res.data.decode())
+        self.assertEqual(result['message'], 'successfully  created user')
+        self.assertEqual(res.status_code, 201)
